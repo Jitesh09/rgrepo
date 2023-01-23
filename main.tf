b9511cb43cb6c5573e2d8d848c47dc027dbbe6e7
Jitesh09 <119019857+Jitesh09@users.noreply.github.com>
2023-01-22 22:36:58 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "example" {
   name     = var.rgname
   location = var.locname
 }
+
+resource "azurerm_public_ip" "pipp" {
+  name                = var.devpip
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
